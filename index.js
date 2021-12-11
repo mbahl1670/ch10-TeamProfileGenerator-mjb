ab8b8ba38981ae6bfd600db6353376adcde2b229
Michael <mbahl1670@gmail.com>
2021-12-11 15:42:40 -0600

    start of index finished, questions will make an array of all the employee ojbects

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const inquirer = require("inquirer");
+// const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
+// array to hold the team data
+const teamInfo = [];
+
+// prompt function to get the manager's info
 const promptManager = () => {
+
+    console.log(`
+    =================
+     Add the Manager
+    =================
+    `);
+
     return inquirer.prompt([
         {
             type: "input",
-            name: "manager",
+            name: "name",
             message: "What is the manager's name?",
             validate: nameInput => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log("Please enter the manager's name!");
+                    console.log("Please enter the manager's name!")
                 }
             }
         },
@@ -50,10 +65,131 @@ const promptManager = () => {
                 }
             }
         }
-    ]);
+    ])
+    .then (managerInfo => {
+        const {name, id, email, officeNumber} = managerInfo;
+        const manager = new Manager(name, id, email, officeNumber);
+
+        teamInfo.push(manager);
+    });
+};
+
+// prompt function to get employee information
+const promptEmployees = () => {
+
+    console.log(`
+    =================
+     Add an Employee
+    =================
+    `);
+
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the employee's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee's name!");
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What is the employee's role?",
+            choices: ["Engineer", "Intern"]
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What it the employee's ID?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee's ID!");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the employee's email?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Pleaes enter the employee's email!");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "gitHub",
+            message: "What is the Engineer's gitHub username?",
+            when: (answers) => answers.role === "Engineer",  // this question is only asked if the role is Engineer
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Engineer's gitHub username!");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the name of the Intern's school?",
+            when: (answers) => answers.role === "Intern", // this question is only asked if the role is Intern
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the name of the Intern's school!");
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "confirmAddEmployee", // confirm status so we know when we are done adding employees
+            message: "Would you like to add another employee?",
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        let {name, id, email, role, confirmAddEmployee, ...roleQuestion} = employeeData;
+        let employee;
+
+        // Create an Engineer Object if 
+        if (role === "Engineer") {
+            employee = new Engineer(name, id, email, roleQuestion);
+        }
+
+        if (role === "Intern") {
+            employee = new Intern(name, id, email, roleQuestion);
+        }
+
+        teamInfo.push(employee);
+
+        if (confirmAddEmployee) {
+            return promptEmployees(teamInfo);
+        } else {
+            return teamInfo;
+        }
+    });
+};
+
+const printDat = () => {
+    // console.log(teamInfo);
+    console.log(teamInfo[1].getGitHub());
 };
 
 promptManager()
-    .then (managerInfo => {
-        console.log(managerInfo);
-    });
+    .then (promptEmployees)
+    .then (printDat) // what we are going to do with the data
+    .catch((err => {
+        console.log(err);
+    }));
