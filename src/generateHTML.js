e5b2b8da2d908b29d5e2a83f8f49582d8254006b
Michael <mbahl1670@gmail.com>
2021-12-17 20:02:02 -0600

    Intern added, added the icons for the different roles

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -14,16 +14,16 @@ const generateManager = managerInfo => {
 
     const managerHTML = empManager.map(managerInfo => {
         return `
-            <div class="card shadow-lg col-3 bg-light">
+            <div class="card shadow-lg col-12 col-sm-6 col-md-4 col-lg-3 m-3 bg-light">
                 <div class="card-header text-white bg-primary mt-4">
                     <h3>${managerInfo.getName()}</h3>
-                    <h4>${managerInfo.getRole()}</h4>
+                    <h4><i class="fas fa-mug-hot"></i>  ${managerInfo.getRole()}</h4>
                 </div>
     
                 <ul class="list-group list-group-flush my-4 border">
-                    <li class="list-group-item bg-white">Id:  ${managerInfo.getID()}</p>
-                    <li class="list-group-item bg-white">Email: <a href="mailto:${managerInfo.getEmail()}">${managerInfo.email}</a></p>
-                    <li class="list-group-item bg-white">Office number: ${managerInfo.getOfficeNumber()}</p>
+                    <li class="list-group-item bg-white">Id:  ${managerInfo.getID()}</li>
+                    <li class="list-group-item bg-white">Email: <a href="mailto:${managerInfo.getEmail()}">${managerInfo.email}</a></li>
+                    <li class="list-group-item bg-white">Office number: ${managerInfo.getOfficeNumber()}</li>
                 </ul>
             </div>
     `;
@@ -43,16 +43,16 @@ const generateEngineers = engineerInfo  => {
 
     const engineerHTML = empEngineers.map(engineerInfo => {
         return `
-            <div class="card shadow-lg col-3 bg-light">
+            <div class="card shadow-lg col-12 col-sm-6 col-md-4 col-lg-3 m-3 bg-light">
                 <div class="card-header text-white bg-primary mt-4">
                     <h3>${engineerInfo.getName()}</h3>
-                    <h4>${engineerInfo.getRole()}</h4>
+                    <h4><i class="fas fa-glasses"></i>  ${engineerInfo.getRole()}</h4>
                 </div>
     
                 <ul class="list-group list-group-flush my-4 border">
-                    <li class="list-group-item bg-white">Id:  ${engineerInfo.getID()}</p>
-                    <li class="list-group-item bg-white">Email: <a href="mailto:${engineerInfo.getEmail()}">${engineerInfo.getEmail()}</a></p>
-                    <li class="list-group-item bg-white">GitHub: <a href="https://github.com/${engineerInfo.getGitHub()}">${engineerInfo.getGitHub()}</a></p>
+                    <li class="list-group-item bg-white">Id:  ${engineerInfo.getID()}</li>
+                    <li class="list-group-item bg-white">Email: <a href="mailto:${engineerInfo.getEmail()}">${engineerInfo.getEmail()}</a></li>
+                    <li class="list-group-item bg-white">GitHub: <a href="https://github.com/${engineerInfo.getGitHub()}">${engineerInfo.getGitHub()}</a></li>
                 </ul>
             </div>
     `;
@@ -62,15 +62,7 @@ const generateEngineers = engineerInfo  => {
 };
 
 const generateInterns = internInfo => {
-
-};
-
-const generateHTML = teamData => {
-    
-
-    
-
-    const empInterns = teamData.filter(employee => {
+    const empInterns = internInfo.filter(employee => {
         if (employee.role === "Intern") {
             return true;
         } else {
@@ -78,6 +70,27 @@ const generateHTML = teamData => {
         }
     });
 
+    const internHTML = empInterns.map(internInfo => {
+        return `
+            <div class="card shadow-lg col-12 col-sm-6 col-md-4 col-lg-3 m-3 bg-light">
+                <div class="card-header text-white bg-primary mt-4">
+                    <h3>${internInfo.getName()}</h3>
+                    <h4><i class="fas fa-user-graduate"></i>  ${internInfo.getRole()}</h4>
+                </div>
+    
+                <ul class="list-group list-group-flush my-4 border">
+                    <li class="list-group-item bg-white">Id:  ${internInfo.getID()}</li>
+                    <li class="list-group-item bg-white">Email: <a href="mailto:${internInfo.getEmail()}">${internInfo.getEmail()}</a></li>
+                    <li class="list-group-item bg-white">School: ${internInfo.getSchool()}</li>
+                </ul>
+            </div>
+    `;
+    });
+
+    return `${internHTML.join("")}`;
+};
+
+const generateHTML = teamData => { 
     return `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -85,6 +98,7 @@ const generateHTML = teamData => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
     <title>Team Profile</title>
 </head>
@@ -98,11 +112,13 @@ const generateHTML = teamData => {
         <div class="row justify-content-center">
             ${generateManager(teamData)}
             ${generateEngineers(teamData)}
+            ${generateInterns(teamData)}
         </div>
     </main>
         
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/257de25400.js" crossorigin="anonymous"></script>  
 </body>
 </html>`
 };
